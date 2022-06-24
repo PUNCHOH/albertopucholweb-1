af2e67a6614d998c382cb27522f2f01948146012
PUNCHOH <46778876+PUNCHOH@users.noreply.github.com>
2022-06-24 17:50:26 +0200

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -9,17 +9,16 @@ export default siteConfig({
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'Alberto Puchol Arcos Web',
+  domain: 'albertopucholweb.vercel.app',
+  author: 'Alberto Puchol Arcos',
 
-  // open graph metadata (optional)
-  description: 'Example Next.js Notion Starter Kit Site',
-
+  // open graph metadata (optional) description: 'This is Alberto Puchol Arcos Web page',
+  
   // social usernames (optional)
-  twitter: 'transitive_bs',
-  github: 'transitive-bullshit',
-  linkedin: 'fisch2',
+  twitter: 'punch__oh',
+  github: 'PUNCHOH',
+  linkedin: 'albertopucholarcos',
 
   // default notion icon and cover images for site-wide consistency (optional)
   // page-specific values will override these site-wide defaults
